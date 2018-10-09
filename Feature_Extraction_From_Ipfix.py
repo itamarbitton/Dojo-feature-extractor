166bb894acd62d0d6081deebf9be66c8bea30f55
itamar <itamarbitton13@gmail.com>
2018-10-09 13:13:45 +0300

    possible split lists by percentage function
    add checks for problematic files

--- Feature_Extraction_From_Ipfix.py
+++ Feature_Extraction_From_Ipfix.py
@@ -43,6 +43,13 @@ unique_percentage = 10
 trn_percentage = 70
 opt_percentage = 0
 tst_percentage = 10
+
+# when analyzing the csv data set we update the lists indexes by the percentage of each data part
+unique_ip_lines_range = []
+trn_data_lines_range = []
+opt_data_lines_range = []
+tst_data_lines_range = []
+
 '''                                            '''
 
 
@@ -78,42 +85,11 @@ def calculate_date_difference(start_datetime, end_datetime):
     return difference.total_seconds() * (10**3)
 
 
-def extract_unique_ip_addresses_from_file(path_to_file, unique_ip_addresses):
-    with open(path_to_file, 'r') as f:
-        field = '"destinationIPv4Address":'
-        for row in f:
-            index = row.index(field)
-            ip = row[index + len(field) + 1: index + len(field) + 1 + row[index + len(field) + 1:].index('"')]
-            unique_ip_addresses.add(ip)
-
-
-def extract_unique_ip_addresses_from_subfolder(path_to_folder, unique_ip_addresses):
-    for subfile in os.listdir(path_to_folder):
-        os.chdir(path_to_folder)
-        if os.path.isdir(subfile):
-            extract_unique_ip_addresses_from_subfolder(path_to_folder + '/' + subfile, unique_ip_addresses)
-        else:
-            extract_unique_ip_addresses_from_file(subfile, unique_ip_addresses)
-
-
-def extract_unique_ip_addresses_from_folder(path_to_folder, output_folder):
-    unique_ip_addresses = set()
-    for subfile in os.listdir(path_to_folder):
-        os.chdir(path_to_folder)
-        if os.path.isdir(subfile):
-            extract_unique_ip_addresses_from_subfolder(path_to_folder + '/' + subfile, unique_ip_addresses)
-        else:
-            extract_unique_ip_addresses_from_file(subfile, unique_ip_addresses)
-
-    if os.path.exists(output_folder):
-        os.remove(output_folder)
-    with open(output_folder, 'w+') as out:
-        for ip in unique_ip_addresses:
-            out.write(ip + '\n')
-
-
 # pre-condition: the function collect_unique_ip_addresses was called and the file of unique ip addresses was created
 # at the unique_ip_report path
+''' NEED TO BE UPDATED   '''
+
+
 def is_ip_address_new(ip_addr):
     with open(unique_ip_report, 'r') as unique_ips:
         ip_list = unique_ips.read().splitlines()
@@ -124,61 +100,65 @@ def is_ip_address_new(ip_addr):
 
 # Function receive a path to a CSV Ipfix file and returns a matrix containing the features of that file
 def extract_features_from_file(path):
-    feature_to_index_dict = {}  # mapping each feature to its column's index in the row
-    return_matrix = []
-    with open(path, 'r') as csv_file:
-        reader = csv.reader(csv_file)
-        headers = next(reader)
-        # in case the file is empty after filtering
-        if not headers:
-            return
-        for index in range(len(headers)):
-            if headers[index] in features_names_in_file.values():
-                feature_to_index_dict[headers[index]] = index
-        index_lst = list(feature_to_index_dict.values())
-        index_lst.sort()
-        for row in reader:
-            initial_features = []
-            for feature in features_names_in_file.values():
-                initial_features.append(row[feature_to_index_dict[feature]])
-            if 'NA' in initial_features:
-                continue
-            # at this point to_add contains the features values by the order they appear in features_names_in_file dict
-            # next part of code create the final list of the features needed to be derived from the existing ones
-            final_feature_list = []
-            # dst-ip (converted to numeric value)
-            final_feature_list.append(int(ipaddress.IPv4Address(initial_features[dst_ip_index])))
-            # is-new (binary)
-            final_feature_list.append(is_ip_address_new(initial_features[dst_ip_index]))
-            # number of packets sent # CHANGE TO INT
-            final_feature_list.append(int(initial_features[num_of_packets_sent_index]))
-            # number of bytes sent
-            final_feature_list.append(initial_features[total_bytes_sent_index])
-            # source port
-            final_feature_list.append(initial_features[src_port_index])
-            # destination port
-            final_feature_list.append(initial_features[dst_port_index])
-            # flags(aka control bits)
-            final_feature_list.append(initial_features[tcp_control_bits_index])
-            # duration of the session (in milliseconds)
-            session_duration = calculate_date_difference(initial_features[flow_start_time_index],
-                                                         initial_features[flow_end_time_index])
-            final_feature_list.append(session_duration)
-            #  part of day
-            # (coding: morning(00:00 - 0 8: 00) = 0, noon(0 8: 00 - 16:00) = 1, night(16: 00 - 00:00) = 2
-            parsed_start_date = parse_date(initial_features[flow_start_time_index])
-            hour = parsed_start_date.hour
-            if 0 <= hour <= 8:
-                part_of_day = 0
-            elif 9 <= hour <= 16:
-                part_of_day = 1
-            else:
-                part_of_day = 2
-            final_feature_list.append(part_of_day)
+    try:
+        feature_to_index_dict = {}  # mapping each feature to its column's index in the row
+        return_matrix = []
+        with open(path, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            headers = next(reader)
+            # in case the file is empty after filtering
+            if not headers:
+                return
+            for index in range(len(headers)):
+                if headers[index] in features_names_in_file.values():
+                    feature_to_index_dict[headers[index]] = index
+            # not all files have all the features we need, in this case we ignore the file
+            if len(feature_to_index_dict) != len(features_names_in_file):
+                return
+            for row in reader:
+                initial_features = []
+                for feature in features_names_in_file.values():
+                    initial_features.append(row[feature_to_index_dict[feature]])
+                if 'NA' in initial_features:
+                    continue
+                # at this point to_add contains the features values by the order they appear in features_names_in_file dict
+                # next part of code create the final list of the features needed to be derived from the existing ones
+                final_feature_list = []
+                # dst-ip (converted to numeric value)
+                final_feature_list.append(int(ipaddress.IPv4Address(initial_features[dst_ip_index])))
+                # is-new (binary)
+                final_feature_list.append(is_ip_address_new(initial_features[dst_ip_index]))
+                # number of packets sent # CHANGE TO INT
+                final_feature_list.append(int(initial_features[num_of_packets_sent_index]))
+                # number of bytes sent
+                final_feature_list.append(initial_features[total_bytes_sent_index])
+                # source port
+                final_feature_list.append(initial_features[src_port_index])
+                # destination port
+                final_feature_list.append(initial_features[dst_port_index])
+                # flags(aka control bits)
+                final_feature_list.append(initial_features[tcp_control_bits_index])
+                # duration of the session (in milliseconds)
+                session_duration = calculate_date_difference(initial_features[flow_start_time_index],
+                                                             initial_features[flow_end_time_index])
+                final_feature_list.append(session_duration)
+                #  part of day
+                # (coding: morning(00:00 - 0 8: 00) = 0, noon(0 8: 00 - 16:00) = 1, night(16: 00 - 00:00) = 2
+                parsed_start_date = parse_date(initial_features[flow_start_time_index])
+                hour = parsed_start_date.hour
+                if 0 <= hour <= 8:
+                    part_of_day = 0
+                elif 9 <= hour <= 16:
+                    part_of_day = 1
+                else:
+                    part_of_day = 2
+                final_feature_list.append(part_of_day)
 
-            return_matrix.append(final_feature_list)
+                return_matrix.append(final_feature_list)
+        return return_matrix
 
-    return return_matrix
+    except Exception as e:
+        print("An error occured when extracting features from: %s \n the error is %s" % (path, e))
 
 
 # NOTICE: this function should only be called from features_to_csv() and therefor the output_report_path file is already
@@ -212,5 +192,48 @@ def features_to_csv(path_to_folder, output_report_path, to_create):
     features_to_csv_subfolder(path_to_folder, output_report_path, writer)
 
 
+def analyze_csv_dataset(path_to_dataset):
+    with open(path_to_dataset, 'r') as csv_dataset:
+        num_of_rows = sum(1 for line in csv_dataset)
+
+
 if __name__ == '__main__':
     features_to_csv('D:/Dojo_data_logs/ipfix-09.2018(filtered)(csv files)', 'D:/Dojo_data_logs/september_dataset.csv', True)
+
+
+
+'''
+from math import ceil
+unique_percentage = 10
+trn_percentage = 70
+opt_percentage = 0
+tst_percentage = 20
+
+unique_ip_lines_range = []
+trn_data_lines_range = []
+opt_data_lines_range = []
+tst_data_lines_range = []
+
+tmp_list = []
+for i in range(1, 250469):
+    tmp_list.append(i)
+
+num_of_rows = len(tmp_list)
+
+unique_ip_lines_range = [0, len(tmp_list[:int((unique_percentage/100)*len(tmp_list))])]
+trn_data_lines_range = [unique_ip_lines_range[1], unique_ip_lines_range[1] + len(tmp_list[unique_ip_lines_range[1]:unique_ip_lines_range[1] + ceil((trn_percentage/100)*len(tmp_list))])]
+opt_data_lines_range = [trn_data_lines_range[1], trn_data_lines_range[1] + len(tmp_list[trn_data_lines_range[1]:trn_data_lines_range[1] + ceil((opt_percentage/100)*len(tmp_list))])]
+tst_data_lines_range = [opt_data_lines_range[1], opt_data_lines_range[1] + len(tmp_list[opt_data_lines_range[1]:opt_data_lines_range[1] + ceil((tst_percentage/100)*len(tmp_list))])]
+
+print(unique_ip_lines_range)
+print(trn_data_lines_range)
+print(opt_data_lines_range)
+print(tst_data_lines_range)
+
+
+
+
+
+'''
+
+
